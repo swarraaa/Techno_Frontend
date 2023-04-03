e7015afb6d2020382d0cd1c3b1004fc66a7727c9
Bhavik143 <113492807+Bhavik143@users.noreply.github.com>
2023-04-03 21:50:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Layout from './components/Layout/Layout'
 import { Route, Router, Routes } from 'react-router'
 import Blogs from './Pages/Blogs'
 import Signupform from './components/Signup/Signupform'
+import Blogf from './components/Blogform/Blogf'
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route exact path='/' element={<Signupform />} />
+        {/* <Route exact path='/' element={<Signupform />} /> */}
         <Route exact path='/signup' element={<Signupform />} />
+        <Route exact path='/blogform' element={<Blogf />} />
       </Routes>
     </Layout>
   )
